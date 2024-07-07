7926d2b91f5b77774e33c825e3314392b20178df
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-07 22:03:46 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // First we read input by making a Buffer for the streaming data
+    let buf_reader = BufReader::new(&mut stream);
+
+    // Parse Request Line from buffer reader
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let strlol = &request_line[..];
+    println!("{}", std::any::type_name_of_val(strlol));
+
+    // If a good request then return status/filename for all others fail
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "http.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "http.html")
+        }
+
+        _ => ("HTTP/1.1 404 Bad Request", "404.html"),
+    };
+
+    // Load up the contents by reading the custom html file
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    // Write the stream data as a response to the client :D
+    stream.write_all(response.as_bytes()).unwrap();
+}
