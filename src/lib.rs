5e4ff80ebd3dcc3daa344b30883e9ba2e1da910a
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2024-07-09 06:15:58 +0200

    Added Worker Arc Mutex to pass Job

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,34 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool
+    ///
+    /// #Panics
+    ///
+    /// The `new` function will panic if the size is zero
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -11,3 +37,19 @@ impl ThreadPool {
     {
     }
 }
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
+    }
+}
+
+struct Job;
